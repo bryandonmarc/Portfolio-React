f9805b5ff928ca816cafd90cb292bcf6e8e15360
bryandonmarc <bryan.donmarc@gmail.com>
2022-09-21 17:30:41 +0800

    fix: change `GithubControl` link

--- components/atoms/HeaderControls/meta.ts
+++ components/atoms/HeaderControls/meta.ts
@@ -1,7 +1,7 @@
 export const meta = {
   kofiLink: "https://ko-fi.com/bryandonmarc",
   linkedInLink: "https://www.linkedin.com/in/donmarc-bryan-salcedo-7169541b6/",
-  githubLink: "https://github.com/bryandonmarc",
+  githubLink: "https://github.com/bryandonmarc/Portfolio-React",
 
   kofiColor: "#13C3FF",
   linkedInColor: "#0A66C2",
