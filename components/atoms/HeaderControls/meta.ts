89116121d32dd966ba1d7c14c7bb7fa28f122d48
bryandonmarc <bryan.donmarc@gmail.com>
2023-02-11 16:13:19 +0800

    chore: update link and resume

--- components/atoms/HeaderControls/meta.ts
+++ components/atoms/HeaderControls/meta.ts
@@ -1,6 +1,6 @@
 export const meta = {
   kofiLink: "https://ko-fi.com/bryandonmarc",
-  linkedInLink: "https://www.linkedin.com/in/donmarc-bryan-salcedo-7169541b6/",
+  linkedInLink: "https://www.linkedin.com/in/donmarc-bryan-s-7169541b6/",
   githubLink: "https://github.com/bryandonmarc/",
   email: "bryan.donmarc@gmail.com",
   phone: "(+63) 976-292-9126",
